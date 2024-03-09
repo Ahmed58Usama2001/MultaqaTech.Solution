5331f918e6cf265017505bd00048508927837bfc
Ahmed Usama <ausama@innotech.com.eg>
2024-03-09 02:03:57 +0200

    Returning comment content and comment id

--- MultaqaTech.APIs/Helpers/MappingProfiles.cs
+++ MultaqaTech.APIs/Helpers/MappingProfiles.cs
@@ -16,7 +16,7 @@ namespace MultaqaTech.APIs.Helpers
             CreateMap<BlogPost, BlogPostToReturnDto>()
                 .ForMember(dest => dest.Category, opt => opt.MapFrom(src => src.Category.Name))
                 .ForMember(dest => dest.Tags, opt => opt.MapFrom(src => src.Tags != null ? src.Tags.Select(s => s.Name).ToList() : null))
-                .ForMember(dest => dest.Comments, opt => opt.MapFrom(src => src.Comments != null ? src.Comments.Select(c => c.CommentContent).ToList() : null))
+                .ForMember(dest => dest.Comments, opt => opt.MapFrom(src => src.Comments != null ? src.Comments.Select(c => new {c.CommentContent, c.Id}).ToList() : null))
                 .ForMember(dest => dest.PublishingDate, opt => opt.MapFrom(src => src.PublishingDate.ToString("dddd, MMMM dd, yyyy 'at' hh:mm:ss tt")))
                  .ForMember(d => d.PictureUrl, O => O.MapFrom<BlogPostPictureUrlResolver>());
 
