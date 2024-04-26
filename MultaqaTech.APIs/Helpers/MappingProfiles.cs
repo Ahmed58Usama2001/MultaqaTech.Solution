079712aec6b9bf40d6f0d92aed50dd989377ba21
Ahmed Usama <ausama@innotech.com.eg>
2024-04-26 14:37:23 +0300

    Refactor Mapping Profiles

--- MultaqaTech.APIs/Helpers/MappingProfiles.cs
+++ MultaqaTech.APIs/Helpers/MappingProfiles.cs
@@ -11,27 +11,29 @@ public class MappingProfiles : Profile
 
         CreateMap<SubjectCreateDto, Subject>().ReverseMap();
 
-        CreateMap<CourseDto, Course>()
-           .ForMember(dest => dest.PrerequisitesIds, opt => opt.MapFrom(src => src.PrerequisitesIds))
-           .ForMember(dest => dest.TagsIds, opt => opt.MapFrom(src => src.TagsIds));
-
-        CreateMap<CurriculumSectionCreateDto, CurriculumSection>();
-        CreateMap<CurriculumSectionUpdateDto, CurriculumSection>();
-        CreateMap<CurriculumSection, CurriculumSectionReturnDto>();
+        #region Blog Post
 
         CreateMap<BlogPostCategoryCreateDto, BlogPostCategory>();
         CreateMap<BlogPostCreateDto, BlogPost>()
             .ForMember(dest => dest.PostPictureUrl, opt => opt.Ignore());
+
         CreateMap<BlogPost, BlogPostToReturnDto>()
             .ForMember(dest => dest.Category, opt => opt.MapFrom(src => src.Category.Name))
             .ForMember(dest => dest.PublishingDate, opt => opt.MapFrom(src => src.PublishingDate.ToString("dddd, MMMM dd, yyyy 'at' hh:mm:ss tt")))
             .ForMember(dest => dest.PictureUrl, opt => opt.MapFrom<GenericMediaUrlResolver<BlogPost, BlogPostToReturnDto>>());
 
-
         CreateMap<BlogPostComment, BlogPostCommentToReturnDto>()
             .ForMember(dest => dest.BlogPost, opt => opt.MapFrom(src => src.BlogPost.Title))
             .ForMember(dest => dest.DatePosted, opt => opt.MapFrom(src => src.DatePosted.ToString("dddd, MMMM dd, yyyy 'at' hh:mm:ss tt")));
-        
+        #endregion
+
+
+        #region Courses
+
+        CreateMap<CourseDto, Course>()
+           .ForMember(dest => dest.PrerequisitesIds, opt => opt.MapFrom(src => src.PrerequisitesIds))
+           .ForMember(dest => dest.TagsIds, opt => opt.MapFrom(src => src.TagsIds));
+
         CreateMap<Course, CourseToReturnDto>()
            .ForMember(dest => dest.Subject, opt => opt.MapFrom(src => src.Subject.Name))
            //.ForMember(dest => dest.Instructor, opt => opt.MapFrom(src => src.Instructor.FirstName + src.Instructor.LastName))
@@ -39,13 +41,24 @@ public class MappingProfiles : Profile
            .ForMember(dest => dest.Prerequisites, opt => opt.MapFrom(src => src.Prerequisites != null ? src.Prerequisites.Select(c => c.Name).ToList() : null))
            .ForMember(d => d.ThumbnailUrl, O => O.MapFrom<GenericMediaUrlResolver<Course, CourseToReturnDto>>());
 
+
         CreateMap<CourseReviewDto, CourseReview>();
         CreateMap<CourseReview, CourseReviewToReturnDto>()
            .ForMember(d => d.ProfilePictureUrl, O => O.MapFrom<GenericMediaUrlResolver<CourseReview, CourseReviewToReturnDto>>());
+
+        CreateMap<CurriculumSectionCreateDto, CurriculumSection>();
+        CreateMap<CurriculumSectionUpdateDto, CurriculumSection>();
+        CreateMap<CurriculumSection, CurriculumSectionReturnDto>();
+
+        #endregion
+
+        #region Zoom meetings
         CreateMap<ZoomMeetingCategoryCreateDto, ZoomMeetingCategory>();
         CreateMap<ZoomMeeting , ZoomMeetingToReturnDto>()
             .ForMember(dest => dest.Category, opt => opt.MapFrom(src => src.Category.Name))
             .ForMember(dest => dest.StartDate, opt => opt.MapFrom(src => src.StartDate.ToString("dddd, MMMM dd, yyyy 'at' hh:mm:ss tt")))
             .ForMember(dest => dest.PictureUrl, opt => opt.MapFrom<GenericMediaUrlResolver<ZoomMeeting, ZoomMeetingToReturnDto>>());
+
+        #endregion
     }
 }
